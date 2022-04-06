938b6221b207f9dc4144576a9617b43022d3fda7
homhom2012z <homhom2012z@gmail.com>
2022-04-07 04:26:28 +0700

    add(users): add link to source code

--- src/layouts/header/index.js
+++ src/layouts/header/index.js
@@ -5,6 +5,7 @@ import {
   Heading,
   Stack,
   useDisclosure,
+  Link as ChakraLink,
 } from "@chakra-ui/react";
 
 import Link from "next/link";
@@ -15,9 +16,15 @@ const Header = () => {
   return (
     <>
       <Flex as="header" width="full" align="center">
-        <Heading as="h1" size="md">
+        <Heading as="h1" size="md" mr={2}>
           <Link href="/">Simple-Native-DB</Link>
         </Heading>
+        <ChakraLink
+          href={"https://github.com/homhom2012z/simple-native-db-client"}
+          target={"_blank"}
+        >
+          source
+        </ChakraLink>
 
         <Box marginLeft="auto">
           <Stack direction={{ base: "row" }} spacing={4}>
